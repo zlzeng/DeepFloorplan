3273c7b06199d47af52786eda8053dd25805df1f
zlzeng <zlzeng6@gmail.com>
2019-07-29 17:41:51 +0800

    update demo code & pretrained model

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Deep Floor Plan Recognition using a Multi-task Network with Room-boundary-Guided Attention
 By Zhiliang ZENG, XIANZHI LI, Ying Kin Yu, and Chi-Wing Fu
 
-coming soon
+[2019/07/29: updated demo code & pretrained model]
+
+## Requirements
+
+- Please install tensorflow-gpu
+- Please install Python 2.7
+- We used Nvidia Titan Xp GPU with CUDA 8.0 installed
+
+Our code have been tested by using tensorflow-gpu==1.10.1
+
+## Python packages
+
+- [numpy]
+- [scipy]
+- [matplotlib]
+- [Pillow]
+
+## demo code usage
+Download our pretrained model and put it into [pretrained] folder, then run
+```bash
+python demo.py --im_path=./demo/45719584.jpg 
+```
---
+++ demo.py
@@ -0,0 +1,87 @@
+import os
+import argparse
+import numpy as np
+import tensorflow as tf
+
+from scipy.misc import imread, imsave, imresize
+from matplotlib import pyplot as plt
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# input image path
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--im_path', type=str, default='./demo/45765448.jpg',
+                    help='input image paths.')
+
+# color map
+floorplan_map = {
+	0: [255,255,255], # background
+	1: [192,192,224], # closet
+	2: [192,255,255], # batchroom/washroom
+	3: [224,255,192], # livingroom/kitchen/dining room
+	4: [255,224,128], # bedroom
+	5: [255,160, 96], # hall
+	6: [255,224,224], # balcony
+	7: [255,255,255], # not used
+	8: [255,255,255], # not used
+	9: [255, 60,128], # door & window
+	10:[  0,  0,  0]  # wall
+}
+
+def ind2rgb(ind_im, color_map=floorplan_map):
+	rgb_im = np.zeros((ind_im.shape[0], ind_im.shape[1], 3))
+
+	for i, rgb in color_map.iteritems():
+		rgb_im[(ind_im==i)] = rgb
+
+	return rgb_im
+
+def main(args):
+	# load input
+	im = imread(args.im_path, mode='RGB')
+	im = im.astype(np.float32)
+	im = imresize(im, (512,512,3)) / 255.
+
+	# create tensorflow session
+	with tf.Session() as sess:
+		
+		# initialize
+		sess.run(tf.group(tf.global_variables_initializer(),
+					tf.local_variables_initializer()))
+
+		# restore pretrained model
+		saver = tf.train.import_meta_graph('./pretrained/pretrained_r3d.meta')
+		saver.restore(sess, './pretrained/pretrained_r3d')
+
+		# get default graph
+		graph = tf.get_default_graph()
+
+		# restore inputs & outpus tensor
+		x = graph.get_tensor_by_name('inputs:0')
+		room_type_logit = graph.get_tensor_by_name('Cast:0')
+		room_boundary_logit = graph.get_tensor_by_name('Cast_1:0')
+
+		# infer results
+		[room_type, room_boundary] = sess.run([room_type_logit, room_boundary_logit],\
+										feed_dict={x:im.reshape(1,512,512,3)})
+		room_type, room_boundary = np.squeeze(room_type), np.squeeze(room_boundary)
+
+		# merge results
+		floorplan = room_type.copy()
+		floorplan[room_boundary==1] = 9
+		floorplan[room_boundary==2] = 10
+		floorplan_rgb = ind2rgb(floorplan)
+
+		# plot results
+		plt.subplot(121)
+		plt.imshow(im)
+		plt.subplot(122)
+		plt.imshow(floorplan_rgb/255.)
+		plt.show()
+
+if __name__ == '__main__':
+	FLAGS, unparsed = parser.parse_known_args()
+	main(FLAGS)
